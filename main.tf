f18fd28bc24925242ff02694559ab72e610cc013
confusdcodr <confusdcodr@users.noreply.github.com>
2020-05-14 10:49:11 -0400

    Adds sleep to principal association to allow invitation to propagate

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ resource "aws_ram_principal_association" "this" {
 
   principal          = var.principal
   resource_share_arn = var.resource_share_arn
+
+  # The invitation sometime takes a few seconds to propagate
+  provisioner "local-exec" {
+    command = "python -c 'import time; time.sleep(5)'"
+  }
 }
 
 resource "aws_ram_resource_share_accepter" "this" {

6a23568bdfe1071f657b885a017c35c44b25eacc
Loren Gordon <lorengordon@users.noreply.github.com>
2024-11-10 09:15:14 -0800

    Removes sleep due to inability to reproduce error in recent aws provider versions

--- modules/cross_account_principal_association/versions.tf
+++ modules/cross_account_principal_association/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.0"
+      version               = ">= 5.37.0"
       configuration_aliases = [aws.owner]
     }
   }
